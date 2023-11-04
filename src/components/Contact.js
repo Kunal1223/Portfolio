d449174f78f7f0e4180da1c621fca0c0bbb53137
Kunal Kishor <kunalbth2002@gmail.com>
2023-11-05 02:01:52 +0530

    Final

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import '../Styles/Contact.css'
-import {BsFacebook , BsInstagram ,BsGithub ,BsLinkedin} from 'react-icons/bs'
+import { BsFacebook, BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs'
 
-import {} from 'react-icons/bs'
+import { } from 'react-icons/bs'
 import ParticlesContainer from '../SubComponents/Particles'
 
 const Contact = () => {
@@ -18,30 +18,38 @@ const Contact = () => {
           <div className="reach">
             <h1>How to Reach Me:</h1>
             <div className="socials">
-               <div className="icons">
-                <a href="https://www.facebook.com/profile.php?id=100008989508430"  > <BsFacebook className='icon'/></a>
-               </div>
+              <a href="https://www.facebook.com/profile.php?id=100008989508430" className='test' >
+                <div className="icons">
+                  <BsFacebook className='icon' />
+                </div>
+              </a>
 
-               <div className="icons">
-                <a href="https://instagram.com/_kunal81?igshid=OGQ5ZDc2ODk2ZA==" > <BsInstagram className='icon'/></a>
-               </div>
+              <a href="https://instagram.com/_kunal81?igshid=OGQ5ZDc2ODk2ZA==" className='test'>
+                <div className="icons">
+                  <BsInstagram className='icon' />
+                </div>
+              </a>
 
-               <div className="icons">
-                <a href="https://github.com/Kunal1223" > <BsGithub className='icon' /></a>
-               </div>
+              <a href="https://github.com/Kunal1223" className='test'>
+                <div className="icons">
+                  <BsGithub className='icon' />
+                </div>
+              </a>
 
-               <div className="icons">
-                <a href="https://www.linkedin.com/in/kunal-kishor-3868a7244/" > <BsLinkedin className='icon'/></a>
-               </div>
+              <a href="https://www.linkedin.com/in/kunal-kishor-3868a7244/" className='test'>
+                <div className="icons">
+                  <BsLinkedin className='icon' />
+                </div>
+              </a>
             </div>
-          </div> 
-        </div> 
+          </div>
+        </div>
 
         <div className="contact-right">
-          <img src="./images/phone.png" alt="" className='phone'/>
+          <img src="./images/phone.png" alt="" className='phone' />
         </div>
         <ParticlesContainer particleColor='#EB4F43' linkColor='#ffff' number="500" />
-      </div>
+      </div >
     </>
   )
 }
